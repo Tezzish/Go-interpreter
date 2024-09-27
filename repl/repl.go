c0dd033c600fcf812229ecf26b0eb451e5796cdc
Tezzish <ishanpahwa14@gmail.com>
2024-09-27 16:41:11 +0200

    feat: adds evaluation of integer and boolean literals as well as prefix evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"Go-interpreter/evaluator"
 	"Go-interpreter/lexer"
 	"Go-interpreter/parser"
 	"bufio"
@@ -12,26 +13,28 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
+		if line == ":q" {
+			break
+		}
 		l := lexer.New(line)
 		p := parser.New(l)
-
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
